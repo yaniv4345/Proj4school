6e8b1637edfb07c47140cb7c1b2f42b6d3699f3b
yaniv4345 <yaniv4345@gmail.com>
2016-11-13 21:05:11 +0200

    Added person to admin

--- Proj/models.py
+++ Proj/models.py
@@ -10,3 +10,6 @@ class person(models.Model):
     address = models.CharField(max_length=70)
     age = models.IntegerField()
     data_added = models.DateField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.name
