37927d3bd43425bf684660365c3014739e6ac097
yaniv4345 <yaniv4345@gmail.com>
2016-11-13 20:39:17 +0200

    Added modules

--- Proj/models.py
+++ Proj/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 
 # Create your models here.
+class person(models.Model):
+    name = models.CharField(max_length=70)
+    id_number = models.IntegerField()
+    address = models.CharField(max_length=70)
+    age = models.IntegerField()
+    data_added = models.DateField(auto_now_add=True, blank=True)
