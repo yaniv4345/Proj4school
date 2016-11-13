ed57bb6a73917bd193d6f2ddf7c101124ae6fc2e
yaniv4345 <yaniv4345@gmail.com>
2016-11-13 22:20:20 +0200

    changed module admin

--- Proj/admin.py
+++ Proj/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import person
 
+class personAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Person Details", {"fields":["name", "id_number", "address", "age", "date_added"]}),
+    ]
+    ordering = ['name']
+    actions = ['delete']
+    readonly_fields = ("date_added",)
+    list_display = ("name","date_added")
+    list_display_links = ("name", "date_added")
+    search_fields = ("name","address","age")
 # Register your models here.
-admin.site.register(person)
+admin.site.register(person,personAdmin)
